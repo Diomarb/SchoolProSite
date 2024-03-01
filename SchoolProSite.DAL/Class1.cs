49c4d3d5397fa15058e84b193998d7977e3cfd28
arian <arian@DESKTOP-8TRUQ82>
2024-03-01 13:21:41 -0400

    Add project files.

---
+++ SchoolProSite.DAL/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace SchoolProSite.DAL
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ SchoolProSite.DAL/Enums/Operations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProSite.DAL.Enums
+{
+    internal class Operations
+    {
+    }
+}
